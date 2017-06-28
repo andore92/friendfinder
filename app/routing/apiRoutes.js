1c743e220be0815f2abcdcd026c388697c172215
Andrew Sadowski <Andrew Sadowski>
2017-06-28 08:32:40 -0400

    populated friendsList array with correct data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,16 +2,52 @@ var express = require('express');
 
 var api = express.Router();
 
-var friendsList = [{
-	name: "Origin Master",
-	phone: "732-234-5678",
-	email: "origin@aol.com",
-	uniqueId: "73111"
-},{
-  name: "Davey D",
-  phone: "732-342-9876",
-  email: "davey@aol.com",
-  uniqueId: "72211"
+var friendsList = [
+	{
+  "name":"Aaron Rodgers",
+  "photo":"http://cdn0.sbnation.com/imported_assets/789745/x2011-August-26-22-2-46.jpg.pagespeed.ic.6yuBNan6w7.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}, {
+  "name":"Superman",
+  "photo":"http://nerdist.com/wp-content/uploads/2017/01/action-comics-cv-977-224713.jpg",
+  "scores":[
+      1,
+      2,
+      5,
+      3,
+      4,
+      2,
+      3,
+      4,
+      5,
+      2
+    ]
+}, {
+  "name":"Spider-man",
+  "photo":"https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+  "scores":[
+      2,
+      5,
+      4,
+      3,
+      2,
+      4,
+      3,
+      5,
+      2,
+      1
+    ]
 }
 ];
 
